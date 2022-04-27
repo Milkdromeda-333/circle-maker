10810454fe07899e627d6fae8d1ae6819d595dcc
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-04-27 11:58:38 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const r = Number(prompt('Enter a number to make a circle'));
+
+const circle = {
+	getCircumference(num) {
+		num = r;
+		circumference = num * (3.14 * 2)
+	console.log(circumference)
+	}
+}
+
+circle.getCircumference();
